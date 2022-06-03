cb8e8f2a7a825789092ba5a999d52b1c0322e27c
huseynhasilci <huseynhasilci@outlook.com>
2022-06-03 09:34:12 +0300

    some changes are made

--- 2021-01/nlp_deneme.py
+++ 2021-01/nlp_deneme.py
@@ -1,5 +1,8 @@
 import json
 import os
+import nltk
+#nltk.download('stopwords')
+#nltk.download('punkt')
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 import numpy as np
@@ -73,10 +76,41 @@ for i in os.listdir():
             elif data['Dairesi'] in real_data.keys():
                 real_data[data['Dairesi']] += 1
 
+def calculate_chi_square_test(word_1, word_2, bigram_words,word_1_counter, word_2_counter, bigram_freq, document_total_size):
+    try:
+        numerator = (document_total_size)*(((bigram_freq*document_total_size)-(word_1_counter*word_2_counter))**2)
+        denominator = (bigram_freq+word_1_counter)*(bigram_freq+word_2_counter)*(word_1_counter+document_total_size)*(word_2_counter+document_total_size)
+        chi_square_result = numerator/denominator
+        print(f'Word-1: {word_1}, Word-1-Size: {word_1_counter}, Word-2: {word_2}, Word-2-Size: {word_2_counter}, Bigram-Words: {bigram_words}, Bigram-Words-Size: {bigram_freq}, Chi-Square-Test {chi_square_result}')
+    except ZeroDivisionError:
+        mutual_value = 1
+
 
 def get_the_constant_values(word_dict):
-    sorted_data = {k: v for k, v in sorted(word_dict.items(), key=lambda item: item[1])}
-
+    word_1_list = [1155, 3076, 2864, 14262, 1085, 7824, 3083, 5035, 4693, 9903]
+    word_2_list = [4693, 3183, 3083, 11003, 297, 8001, 3297, 7824, 3491, 8400]
+    bigr
+    sorted_data = {k: v for k, v in sorted(word_dict.items(), key=lambda item: item[1],reverse=True)}
+    print(sorted_data['vicdani'])
+    print(sorted_data['kanının'])
+    print(sorted_data['sürecini'])
+    print(sorted_data['yansıtan'])
+    print(sorted_data['bölge'])
+    print(sorted_data['adliye'])
+    print(sorted_data['oy'])
+    print(sorted_data['birliğiyle'])
+    print(sorted_data['tetkik'])
+    print(sorted_data['hakimi'])
+    print(sorted_data['yürürlüğe'])
+    print(sorted_data['giren'])
+    print(sorted_data['adliye'])
+    print(sorted_data['mahkemesi'])
+    print(sorted_data['yayımlanarak'])
+    print(sorted_data['yürürlüğe'])
+    print(sorted_data['kanının'])
+    print(sorted_data['oluştuğu'])
+    print(sorted_data['gününde'])
+    print(sorted_data['oybirliğiyle'])
     for_std_val_list = []
     for i in sorted_data.keys():
         if i == '...' or i == '' or i == ', ':
@@ -89,12 +123,13 @@ def get_the_constant_values(word_dict):
     mean = calculate_mean(for_std_val_list)
     total_document = calculate_total_document(for_std_val_list)
     variance = calculate_variance(for_std_val_list)
-    print(standard_deviation)
-    print(mean)
-    print(total_document)
-    print(variance)
-    print(unique_words)
+    #print(standard_deviation)
+    #print(mean)
+    #print(total_document)
+    #print(variance)
+    #print(unique_words)
 
+#print(real_data)
 
 get_the_constant_values(real_data)
 print(stopwords.words('turkish'))
